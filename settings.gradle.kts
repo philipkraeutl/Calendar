d06671784abe4192ebce45f9830309deb00c307a
swapindodeveloper <126358275+swapindodeveloper@users.noreply.github.com>
2024-12-04 08:58:53 +0100

    Update settings.gradle.kts

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -10,6 +10,7 @@ pluginManagement {
         maven {
             setUrl("https://plugins.gradle.org/m2/")
         }
+		maven("https://maven.pkg.jetbrains.space/public/p/compose/dev")
     }
 }
 
@@ -20,6 +21,8 @@ dependencyResolutionManagement {
         maven {
             setUrl("https://plugins.gradle.org/m2/")
         }
+ 		maven("https://maven.pkg.jetbrains.space/public/p/compose/dev")
+        maven("https://maven.pkg.jetbrains.space/kotlin/p/wasm/experimental")
     }
 
 }
